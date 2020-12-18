8a160b6e175157c1b733b2dedef376973e1e8af9
ugur ozoz <ugurozoz@yahoo.com>
2020-12-19 00:21:27 +0300

    Thesis page is on progress

--- zehrakaratas.art/src/Containers/Thesis/ThesisContent/ThesisContent.js
+++ zehrakaratas.art/src/Containers/Thesis/ThesisContent/ThesisContent.js
@@ -1,60 +1,71 @@
 import React from 'react';
 import './ThesisContent.css';
+import ThesisImages from './ThesisImages/ThesisImages';
 
 const thesisContent = (props) => {
   return (
     <div className='container thesis-container'>
-      <p>
-        Fotoğraf makinasının bulunması sanatın katı kurallarından kurtulmasına
-        izin verecek bir ortam yaratmıştır. Ekpresyonizm bu görüntüler tarihinde
-        yeni bir dönüm noktası olmuştur. .
-      </p>
-      <p>
-        Ekpresyonizimle yeni bir insanın doğuşu veriliyordu. Kendini anlatacak
-        bir yol buluyordu. Savaşların insanlar üzerinde yarattığı gerilim 20. yy
-        ın yoğun geçen temposunda güven ve güvensizliği anlatmanın bir yoluydu.
-      </p>
-      <p>
-        Kapitalizmin, insan doğasını parçaladığını ve teknolojik düzeniyle
-        giderek yayılan psişik bir sağlıksızlık barındırıyordu.
-      </p>
-      <p>
-        Dışarıdan görünen güzel yüzlerimizin altında yatan travmaları yansıtan
-        yüzler yapmak istiyorum.
-      </p>
-      <p>
-        Önemli olan mermer gibi pürüzsüz yüzler yapmak değil; bir duygu durumunu
-        ifade etmektir. İfadesiz bir anla diğer bir an arasında giderken,
-        yüzünün atomlarının aldığı hal, tarif edilmez yaratımlar sunar
-        sanatçıya.
-      </p>
+      <strong className='gen-head'>İfadeler</strong>
+      <div className='thesis-container__inner'>
+        <div className='thesis-text'>
+          <p>
+            Fotoğraf makinasının bulunması sanatın katı kurallarından
+            kurtulmasına izin verecek bir ortam yaratmıştır. Ekpresyonizm bu
+            görüntüler tarihinde yeni bir dönüm noktası olmuştur. .
+          </p>
+          <p>
+            Ekpresyonizimle yeni bir insanın doğuşu veriliyordu. Kendini
+            anlatacak bir yol buluyordu. Savaşların insanlar üzerinde yarattığı
+            gerilim 20. yy ın yoğun geçen temposunda güven ve güvensizliği
+            anlatmanın bir yoluydu.
+          </p>
+          <p>
+            Kapitalizmin, insan doğasını parçaladığını ve teknolojik düzeniyle
+            giderek yayılan psişik bir sağlıksızlık barındırıyordu.
+          </p>
+          <p>
+            Dışarıdan görünen güzel yüzlerimizin altında yatan travmaları
+            yansıtan yüzler yapmak istiyorum.
+          </p>
+          <p>
+            Önemli olan mermer gibi pürüzsüz yüzler yapmak değil; bir duygu
+            durumunu ifade etmektir. İfadesiz bir anla diğer bir an arasında
+            giderken, yüzünün atomlarının aldığı hal, tarif edilmez yaratımlar
+            sunar sanatçıya.
+          </p>
 
-      <p>
-        Hayat mermer kaideler gibi pürüzsüz değildir. Sanatçı da yaşadığı çağdan
-        bağımsız ve ayrıksı düşünülemez. Savaşlar, soykırımlar ve ne varsa
-        yüzyıla dair hepsi bir bütün olarak yaşanır . Hele bir de son sürat
-        devleşen teknolajinin dışında olmaya çalışmak, istesekte yapacağımız bir
-        şey değildir artık. Bütün bunlar yaşanırken dört bir köşesinde
-        evrenimizin, huzurlu bir gökyüzü altında uzanamazsınız. Ya da bir
-        sanatçının uzanmasını bekleyemezsiniz. Varsayalım ki o göğün altında
-        uzanıyormuş gibi yapıyorsunuz, neredeyse her şey kendi ahenginde
-        akıyormuş gibi... Sebebini bilmediğiniz , bir türlü anlayamadığınız
-        kasılmalarınız , baş ağrılarınız, anlamlandıramadığınız tikleriniz,
-        kaygılarınız başlar ve bu liste böyece uzar gider.
-      </p>
-      <p>
-        Siz, coğrafyanızın size dayattığı öğrenmişliğin dışına ne kadar
-        istesenizde çıkamazsınız. Elbette dener, farkındalığınızı yükseltmeye
-        çalışırsınız. Ama tüm bu deneyimler ışığında kendinizi, otellerde
-        eşantiyon olarak verilen bir sıkımlık dişmacunu değerinde görmeye
-        başlarsınız.
-      </p>
+          <p>
+            Hayat mermer kaideler gibi pürüzsüz değildir. Sanatçı da yaşadığı
+            çağdan bağımsız ve ayrıksı düşünülemez. Savaşlar, soykırımlar ve ne
+            varsa yüzyıla dair hepsi bir bütün olarak yaşanır . Hele bir de son
+            sürat devleşen teknolajinin dışında olmaya çalışmak, istesekte
+            yapacağımız bir şey değildir artık. Bütün bunlar yaşanırken dört bir
+            köşesinde evrenimizin, huzurlu bir gökyüzü altında uzanamazsınız. Ya
+            da bir sanatçının uzanmasını bekleyemezsiniz. Varsayalım ki o göğün
+            altında uzanıyormuş gibi yapıyorsunuz, neredeyse her şey kendi
+            ahenginde akıyormuş gibi... Sebebini bilmediğiniz , bir türlü
+            anlayamadığınız kasılmalarınız , baş ağrılarınız,
+            anlamlandıramadığınız tikleriniz, kaygılarınız başlar ve bu liste
+            böyece uzar gider.
+          </p>
+          <p>
+            Siz, coğrafyanızın size dayattığı öğrenmişliğin dışına ne kadar
+            istesenizde çıkamazsınız. Elbette dener, farkındalığınızı
+            yükseltmeye çalışırsınız. Ama tüm bu deneyimler ışığında kendinizi,
+            otellerde eşantiyon olarak verilen bir sıkımlık dişmacunu değerinde
+            görmeye başlarsınız.
+          </p>
 
-      <p>
-        Ve hiç bir şey öylesine gelip geçmez yanınızdan. Bir varsınız, bir
-        yoksunuz. Ama insana ait yaşanmışlıklarımız yanıbaşımızda genetiğimize
-        işleyerek büyür.
-      </p>
+          <p>
+            Ve hiç bir şey öylesine gelip geçmez yanınızdan. Bir varsınız, bir
+            yoksunuz. Ama insana ait yaşanmışlıklarımız yanıbaşımızda
+            genetiğimize işleyerek büyür.
+          </p>
+        </div>
+        <div className='thesis-image'>
+          <ThesisImages />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ zehrakaratas.art/src/Containers/Thesis/ThesisContent/ThesisImages/ThesisImages.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const imageList = [
+  '1-1.jpg',
+  '1-2.jpg',
+  '2-0.jpg',
+  '2-1.jpg',
+  '3.jpg',
+  '4-1.jpg',
+  '4-2.jpg',
+  '5-2.jpg',
+  '6-1.jpg',
+];
+
+
+
+const thesisImages = (props) => {
+  return <div style={{ background: '#ff0' }}>XX</div>;
+};
+
+export default thesisImages;
